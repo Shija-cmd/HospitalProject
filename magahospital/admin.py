162cb11944062cfde95e1f812db78b066f9cdc88
Shija-cmd <shijajuma@protonmail.com>
2026-05-05 12:58:51 +0300

    Edited the admin panel

--- magahospital/admin.py
+++ magahospital/admin.py
@@ -7,3 +7,7 @@ admin.site.register(Doctor_one)
 admin.site.register(Lab)
 admin.site.register(Med_Prescription)
 admin.site.register(Dispense_Medics)
+
+admin.site.site_header = "Admin Panel"
+admin.site.site_title = "Maga Hospital Admin"
+admin.site.index_title = "Welcome to Maga Hospital Admin"
