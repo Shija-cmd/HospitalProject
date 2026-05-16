591b967c9eaf267ef44849a5603d3b6d9e152326
Shija-cmd <shijajuma@protonmail.com>
2026-05-17 01:01:48 +0300

    Making the forms nicey

--- magahospital/views.py
+++ magahospital/views.py
@@ -643,26 +643,26 @@ def visit_report_pdf(request, visit_id):
         'visit': visit,
 
         'doctor': getattr(
-        visit,
+            visit,
         'doctor',
         None
     ),
 
         'labs': list(
-        visit.labs.all()
+            visit.labs.all()
     ),
 
         'prescriptions': list(
-        visit.prescriptions.all()
+            visit.prescriptions.all()
     ),
 
         'dispenses': list(
-        visit.dispenses.all()
+            visit.dispenses.all()
     ),
 
-    'generated_by': request.user,
+        'generated_by': request.user,
 
-})
+    })
 
     response = HttpResponse(
         content_type='application/pdf'
