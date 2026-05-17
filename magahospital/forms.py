f1e6a9700df7547b6eaedd0bbc989b28acf35ba5
Shija-cmd <shijajuma@protonmail.com>
2026-05-17 20:35:36 +0300

    Making payment module

--- magahospital/forms.py
+++ magahospital/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth.models import User
 
-from .models import Patient, Doctor, Lab, Prescription, Dispense
+from .models import Patient, Doctor, Lab, Prescription, Dispense, Bill
 
 
 # =========================
