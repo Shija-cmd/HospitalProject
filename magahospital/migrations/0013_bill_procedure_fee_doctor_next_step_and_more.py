06ee409c8ea3f3da3c619f442c9ec35efc41f0dc
Shija-cmd <shijajuma@protonmail.com>
2026-05-19 17:42:51 +0300

    Fixed migrations and billing workflow

--- magahospital/migrations/0013_bill_procedure_fee_doctor_next_step_and_more.py
+++ magahospital/migrations/0013_bill_procedure_fee_doctor_next_step_and_more.py
@@ -15,11 +15,7 @@ class Migration(migrations.Migration):
             name='procedure_fee',
             field=models.DecimalField(decimal_places=2, default=0, max_digits=10),
         ),
-        migrations.AddField(
-            model_name='doctor',
-            name='next_step',
-            field=models.CharField(choices=[('Lab', 'Send To Laboratory'), ('Cashier', 'Send To Cashier')], default='Cashier', max_length=20),
-        ),
+        
         migrations.AddField(
             model_name='doctor',
             name='procedure_name',
