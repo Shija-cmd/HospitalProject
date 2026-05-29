fba9714d783c84f342f322aff413700fe7ee8a11
Shija-cmd <shijajuma@protonmail.com>
2026-05-30 00:56:46 +0300

    Updating tests requests form

--- project/settings.py
+++ project/settings.py
@@ -44,11 +44,17 @@ ALLOWED_HOSTS = config(
 
 # CSRF trusted origins
 CSRF_TRUSTED_ORIGINS = [
+
     "http://127.0.0.1:8000",
+
     "http://localhost:8000",
+
+    "https://hospitalproject-3s82.onrender.com",
+
     "https://shijajuma.website",
+
     "https://www.shijajuma.website",
-    "https://*.onrender.com",
+
 ]
 
 
